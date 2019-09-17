c34fe10cda411e51fb02d8086e97bcbfdaff6357
Maksim Shirshikov <iammaksim2000shirshikov@gmail.com>
2019-09-17 15:18:30 +0300

    'logic'

--- js/script.js
+++ js/script.js
@@ -1,15 +1,58 @@
-// const 
-// 		rang 	= document.querySelector("#v1"),
-// 		answ 	= document.querySelector("#answ");
+const 
+		result = document.querySelector(".result"),
+		ranges = document.querySelectorAll('#answ'),
+		rves = document.querySelectorAll(".rves"),
 
-// rang.addEventListener('mousemove', function() {
-// 	answ.value = rang.value;
-// });
+		constantes = {
+			litr: 6,
+			bottle: 5
+		},
+		variants =  ['банка краски', 'банки красок', 'банок краски'];
 
-// answ.addEventListener('keydown', function() {
-// 	rang.value = answ.value;
-// });
+function getResult(eles) {
+	let 
+		text  = '',
+		array = [],
+		count = 0,
+		len_h = 0,
+		h_wid = 0;
 
-// answ.addEventListener('keyup', function() {
-// 	rang.value = answ.value;
-// });
+	eles.forEach(el => {
+		if (el.value != '') {
+			array.push(parseFloat(el.value));
+		}
+	});
+	len_h = 2 * array[0] * array[2];
+	h_wid = 2 * array[1] * array[2];
+	count = Math.ceil((len_h + h_wid) / constantes.litr / constantes.bottle);
+	text += 'Вам потребуется ';
+	text += count + ' ';
+	if (count % 100 >= 11 && count % 100 < 15) {
+		text += variants[2];
+	}
+	else if (count % 10 === 1) {
+		text += variants[0];
+	}
+	else if (count % 10 === 2 || count % 10 === 3 || count % 10 === 4) {
+		text += variants[1];
+	}
+	else {
+		text += variants[2];
+	}
+	result.textContent = text;
+}
+
+ranges.forEach(el => {
+	el.addEventListener('keydown', function() {
+		getResult(ranges);
+	});
+	el.addEventListener('keyup', function() {
+		getResult(ranges);
+	});
+});
+
+rves.forEach(rve => {
+	rve.addEventListener('change', function() {
+		getResult(rves);
+	})
+});
--- vue/app.js
+++ vue/app.js
@@ -2,7 +2,7 @@ const len = new Vue({
 	el: ".len",
 	data: {
 		value: "Длина",
-		answ: 0
+		answ: 1
 	}
 });
 
@@ -10,7 +10,7 @@ const wid = new Vue({
 	el: ".wid",
 	data: {
 		value: "Ширина",
-		answ: 0
+		answ: 1
 	}
 });
 
@@ -18,21 +18,6 @@ const height = new Vue({
 	el: ".height",
 	data: {
 		value: "Высота",
-		answ: 0
-	}
-});
-
-const btn = new Vue({
-	el: ".btn",
-	data: {
-		litr: [
-			'литр',
-			'литров'
-		]
-	},
-	computed: {
-		getResult: function() {
-			
-		}
+		answ: 1
 	}
 });
